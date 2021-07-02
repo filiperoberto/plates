569536994a1a052c4b7267d64c04011da08a4e69
filiperoberto <filiperoberto.s@gmail.com>
2021-07-02 20:52:55 -0300

    1 file modified

--- src/components/Plate.vue
+++ src/components/Plate.vue
@@ -1,6 +1,7 @@
 <template>
   <div class='plate'>
-    {{plate}}
+    <span>{{letters}}</span>
+    <span>{{numbers}}</span>
   </div>
 </template>
 
@@ -8,11 +9,30 @@
 export default {
   props: {
     plate: String
+  },
+  computed: {
+    letters() {
+      return this.plate.slice(0,3)
+    },
+    numbers() {
+      return this.plate.slice(3,8)
+    }
   }
 }
 </script>
 <style scoped>
   .plate {
+    box-sizing: border-box;
     width: 100%;
+    border: 0.2em solid #101c36;
+    border-radius: 0.2em;
+    text-align:center;
+    font-size: 4em;
+    padding:0.4em;
+    letter-spacing: 0.1em;
+  }
+
+  .plate > span {
+    display: inline-block;
   }
 </style>
