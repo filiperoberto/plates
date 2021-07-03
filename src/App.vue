73fa5b62a7de30616f6b0b848eb1cd89b09dd411
filiperoberto <filiperoberto.s@gmail.com>
2021-07-03 19:02:57 -0300

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <plate v-for='placa in placas' :plate='placa.placa' :type='placa.tipo' class='placa'/>
+    <plate v-for='placa in placas' :plate='placa.placa' :type='placa.tipo' class='placa' :uf='placa.uf' :municipio='placa.municipio' :moto='placa.moto'/>
   </div>
 </template>
 
@@ -15,7 +15,7 @@ export default {
   data() {
     return {
       placas: [
-        { placa : 'MJL9746', tipo: 'particular'},
+        { placa : 'MJL9746', tipo: 'particular', uf:'sc', municipio: 'florianópolis'},
         { placa : 'MJL9746', tipo: 'comercial'},
         { placa : 'MJL9746', tipo: 'especial'},
         { placa : 'MJL9746', tipo: 'oficial'},
@@ -26,7 +26,19 @@ export default {
         { placa : 'MJL9H46', tipo: 'especial'},
         { placa : 'MJL9H46', tipo: 'oficial'},
         { placa : 'MJL9H46', tipo: 'colecionador'},
-        { placa : 'MJL9H46', tipo: 'diplomatico'}
+        { placa : 'MJL9H46', tipo: 'diplomatico'},
+        { placa : 'MJL9746', tipo: 'particular', uf:'sc', municipio: 'florianópolis', moto: true},
+        { placa : 'MJL9746', tipo: 'comercial', moto: true},
+        { placa : 'MJL9746', tipo: 'especial', moto: true},
+        { placa : 'MJL9746', tipo: 'oficial', moto: true},
+        { placa : 'MJL9746', tipo: 'colecionador', moto: true},
+        { placa : 'MJL9746', tipo: 'diplomatico', moto: true},
+        { placa : 'MJL9H46', tipo: 'particular', moto: true},
+        { placa : 'MJL9H46', tipo: 'comercial', moto: true},
+        { placa : 'MJL9H46', tipo: 'especial', moto: true},
+        { placa : 'MJL9H46', tipo: 'oficial', moto: true},
+        { placa : 'MJL9H46', tipo: 'colecionador', moto: true},
+        { placa : 'MJL9H46', tipo: 'diplomatico', moto: true}
       ]
     }
   }
